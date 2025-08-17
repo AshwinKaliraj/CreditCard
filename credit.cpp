9b1bbe80de140427ef2c8ef6cb26e1a71347e53a
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-18 00:17:45 +0530

    2

--- credit.cpp
+++ credit.cpp
@@ -14,3 +14,22 @@ vector<Customer> customerDB = {
     {"MSD", "7", 20000.0},
     {"Hardik", "33", 1000.0}
 };
+bool cardValidation(string &cardNumber, double amount) {
+    cout << "\nPayment verified by card network" << endl;
+
+    for (auto &c : customerDB) {
+        if (c.cardNumber == cardNumber) {
+            cout << "Card is valid" << endl;
+            if (c.balance >= amount) {
+                cout << "Funds available" << endl;
+                cout << "Payment verified by bank" << endl;
+                return true;
+            } else {
+                cout << "Insufficient funds" << endl;
+                return false;
+            }
+        }
+    }
+    cout << "Invalid card" << endl;
+    return false;
+}
